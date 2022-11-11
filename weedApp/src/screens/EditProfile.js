0ddc473bbb10acd8089d753de8a5b4b7561fabc4
Octavio A. Celaya <octavio.celaya@karimnot.com>
2022-11-11 17:47:30 -0600

    added new icons

--- weedApp/src/screens/EditProfile.js
+++ weedApp/src/screens/EditProfile.js
@@ -50,7 +50,7 @@ export default function EditProfile() {
         //console.log(result)
         if (result) {
             setUsu(JSON.parse(result));
-            await traerFoto();
+            //await traerFoto();
         }
         } catch (error) {
             console.log(error)
@@ -83,7 +83,7 @@ export default function EditProfile() {
             >
                 <ContainerComponent>
                     <Image
-                        source={{ uri: imagen }}
+                        source={{ uri: `https://mexicoamparame.s3.us-west-2.amazonaws.com/${usu?.imagen}?AWSAccessKeyId=AKIAV5XFJVPABQZOHZR7&Expires=1668303522&Signature=ruvgxDL2mlvPOFgjqRkfhhR8YQk%3D` }}
                         style={{
                             width: 100,
                             height: 100,
--- weedApp/src/screens/MainLayout.js
+++ weedApp/src/screens/MainLayout.js
@@ -9,79 +9,77 @@ import WishList from "./WishList";
 import Profile from "./Profile";
 
 import {
-    HomeTab,
-    SearchTab,
-    WishListTab,
-    ProfileTab,
-    TabElement,
-    Bag,
+  HomeTab,
+  SearchTab,
+  WishListTab,
+  ProfileTab,
+  TabElement,
+  Bag,
 } from "../svg";
 import { COLORS, FONTS, cartItems, products } from "../constants";
+import { Ionicons } from "@expo/vector-icons";
 
 export default function MainLayout() {
-    const navigation = useNavigation();
-    const [selectedTab, setSelectedTab] = useState("Home");
+  const navigation = useNavigation();
+  const [selectedTab, setSelectedTab] = useState("Home");
 
-    const tabs = [
-        {
-            id: "1",
-            screen: "Home",
-            icon: (
-                <HomeTab
-                    color={
-                        selectedTab == "Home" ? COLORS.golden : COLORS.lightGray
-                    }
-                />
-            ),
-        },
-        {
-            id: "2",
-            screen: "Search",
-            icon: (
-                <SearchTab
-                    color={
-                        selectedTab == "Search"
-                            ? COLORS.golden
-                            : COLORS.lightGray
-                    }
-                />
-            ),
-        },
-        {
-            id: "3",
-            screen: "Order",
-            icon: (
-                <View
-                    style={{
-                        width: 58,
-                        height: 58,
-                        //borderWidth: 0.2,
-                        //borderColor: "#BBA36B",
-                        top: -10,
-                        borderRadius: 30,
-                        justifyContent: "center",
-                        alignItems: "center",
-                    }}
-                >
-                    <View
-                        style={{
-                            width: 54,
-                            height: 54,
-                            backgroundColor: COLORS.goldenTransparent_01,
-                            borderRadius: 30,
-                            justifyContent: "center",
-                            alignItems: "center",
-                        }}
-                    >
-                        {//<Bag />
-                        }
-                        <Image
-                            style={{ width: 25, height: 25  }}
-                            resizeMethod="auto"
-                            resizeMode="stretch"
-                            source={require('../assets/images/logopeque.png')}
-                        />
-                        {/* <View
+  const tabs = [
+    {
+      id: "1",
+      screen: "Home",
+      icon: (
+        <HomeTab
+          color={selectedTab == "Home" ? COLORS.golden : COLORS.lightGray}
+        />
+      ),
+    },
+    {
+      id: "2",
+      screen: "Search",
+      icon: (
+        <Ionicons
+          name="grid-outline"
+          size={25}
+          color={selectedTab == "Search" ? COLORS.golden : COLORS.lightGray}
+        />
+      ),
+    },
+    {
+      id: "3",
+      screen: "Order",
+      icon: (
+        <View
+          style={{
+            width: 58,
+            height: 58,
+            //borderWidth: 0.2,
+            //borderColor: "#BBA36B",
+            top: -10,
+            borderRadius: 30,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <View
+            style={{
+              width: 54,
+              height: 54,
+              backgroundColor: COLORS.goldenTransparent_01,
+              borderRadius: 30,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            {
+              //<Bag />
+            }
+            <Image
+              style={{ width: 25, height: 25 }}
+              resizeMethod="auto"
+              resizeMode="stretch"
+              source={require("../assets/images/logopeque.png")}
+            />
+            {/* <View
                             style={{
                                 width: 18,
                                 height: 18,
@@ -104,80 +102,81 @@ export default function MainLayout() {
                                 5
                             </Text>
                             </View>*/}
-                    </View>
-                </View>
-            ),
-        },
-        {
-            id: "4",
-            screen: "WishList",
-            icon: (
-                <WishListTab
-                    color={
-                        selectedTab == "WishList"
-                            ? COLORS.golden
-                            : COLORS.lightGray
-                    }
-                />
-            ),
-        },
-        {
-            id: "5",
-            screen: "Profile",
-            icon: (
-                <ProfileTab
-                    color={
-                        selectedTab == "Profile"
-                            ? COLORS.golden
-                            : COLORS.lightGray
-                    }
-                />
-            ),
-        },
-    ];
-
-    return (
-        <View style={{ flex: 1, backgroundColor: COLORS.goldenTransparent_01 }}>
-            {selectedTab == "Home" && <Home />}
-            {selectedTab == "Search" && <Search />}
-            {selectedTab == "Order" && <CartIsEmpty />}
-            {selectedTab == "WishList" && <WishList />}
-            {selectedTab == "Profile" && <Profile />}
-
-            <View
-                style={{
-                    alignSelf: "center",
-                    position: "absolute",
-                    bottom: 68,
-                }}
-            >
-                <TabElement />
-            </View>
-            <View
-                style={{
-                    flexDirection: "row",
-                    justifyContent: "space-between",
-                    alignItems: "center",
-                    paddingHorizontal: 28,
-                    backgroundColor: COLORS.white,
-                    paddingBottom: 10,
-                }}
-            >
-                {tabs.map((item, index) => {
-                    return (
-                        <TouchableOpacity
-                            key={index}
-                            onPress={() =>
-                                item.screen == "Order" && products.length !== 0
-                                    ? navigation.navigate("Order")
-                                    : setSelectedTab(item.screen)
-                            }
-                        >
-                            {item.icon}
-                        </TouchableOpacity>
-                    );
-                })}
-            </View>
+          </View>
         </View>
-    );
+      ),
+    },
+    {
+      id: "4",
+      screen: "WishList",
+      icon: (
+        // <WishListTab
+        //     color={
+        //         selectedTab == "WishList"
+        //             ? COLORS.golden
+        //             : COLORS.lightGray
+        //     }
+        // />
+        <Ionicons
+          name="reader-outline"
+          size={25}
+          color={selectedTab == "WishList" ? COLORS.golden : COLORS.lightGray}
+        />
+      ),
+    },
+    {
+      id: "5",
+      screen: "Profile",
+      icon: (
+        <ProfileTab
+          color={selectedTab == "Profile" ? COLORS.golden : COLORS.lightGray}
+        />
+      ),
+    },
+  ];
+
+  return (
+    <View style={{ flex: 1, backgroundColor: COLORS.goldenTransparent_01 }}>
+      {selectedTab == "Home" && <Home />}
+      {selectedTab == "Search" && <Search />}
+      {selectedTab == "Order" && <CartIsEmpty />}
+      {selectedTab == "WishList" && <WishList />}
+      {selectedTab == "Profile" && <Profile />}
+
+      <View
+        style={{
+          alignSelf: "center",
+          position: "absolute",
+          bottom: 68,
+        }}
+      >
+        <TabElement />
+      </View>
+      <View
+        style={{
+          flexDirection: "row",
+          justifyContent: "space-between",
+          alignItems: "center",
+          paddingHorizontal: 28,
+          backgroundColor: COLORS.white,
+          paddingBottom: 10,
+        }}
+      >
+        {tabs.map((item, index) => {
+          return (
+            <TouchableOpacity
+              key={index}
+              onPress={() =>
+                item.screen == "Order" && products.length !== 0
+                  ? navigation.navigate("Order")
+                  : setSelectedTab(item.screen)
+              }
+            >
+              {item.icon}
+            </TouchableOpacity>
+          );
+        })}
+      </View>
+    </View>
+  );
 }
