94d054855da119a84021723f07f73e6b3446de5b
Jairo Esteban Martínez Portillo <jairoboss00@gmail.com>
2022-11-28 00:47:05 -0600

    change ipp

--- weedApp/src/service/PacienteService.js
+++ weedApp/src/service/PacienteService.js
@@ -3,7 +3,7 @@ const prefix = "/pacientes";
 
 export default class PacienteService {
   static async create(data) {
-    return (await httpClient.post(`${prefix}`, data)).data;
+    return (await httpClient.post(`${prefix}/mobile`, data)).data;
   }
 
   static async update(data) {
